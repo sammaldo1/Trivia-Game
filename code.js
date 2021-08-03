95d44c04ed349c07610302604414ddca1753d490
Samantha <sammal729@gmail.com>
2021-08-03 14:49:59 -0400

    fetch successful , need to troubleshoot functions

--- code.js
+++ code.js
@@ -2,89 +2,119 @@
 // and select "Open with Live Server"
 
 // YOUR CODE HERE!
-//create 2 pages view One & view 2
-//View One should be where the game starts
-    //includes page title h1
-    //start game button add text "Let's play"
-    //after start game button is clicked should take to View 2
-    //remove submit button and append everything else(one option), wrap button in a a<> with the html , or by using window.location.
-//View 2:
-//should display to HTML: 
-    //title using h1
-    //category using h2
-    //total points using <p>
-    //questaions using div id="questions"
-    //answers box using input box with  div id="answers"
-    //submit with button (creat an event listener that checks users answer)
 
-//combining JS to HTML:
-//questions using div id="questions"
+
+let totalScore = 0;
+let getTotalScoreToHTML = document.getElementById("totalPoints")
+let getAnswerFromHTML = document.querySelector(answers);
+let getQuestionFromHTML = document.getElementById("questions");
+let getCategoryFromHTML = document.getElementById("categoryTitle");
+let submitButton = document.getElementById("submitAnswer");
+
 //use fetch to pull questions from the api 
-    //use a api that doesn't require an API key
-    //use math.random()
-    //questions will come from a single category, category must be random but only one category
-    //get a categories ID from a random question to use a parameter which gives questions in a particular category
-    //once random category is selected the questions will be randomly chosen as well. 
+//use a api that doesn't require an API key
+//use math.random()
+//questions will come from a single category, category must be random but only one category
+//get a categories ID from a random question to use a parameter which gives questions in a particular category
+//once random category is selected the questions will be randomly chosen as well. 
 //questions using div id="questions"
 //present a single q to the user
-    //when game begins a question will appear on the HTML, the questions & all next q's after will belong to the same category
-    //CANT be an alert or prompt, will use <p> tag
+//when game begins a question will appear on the HTML, the questions & all next q's after will belong to the same category
+//CANT be an alert or prompt, will use <p> tag
 
 
 //allow the user to respond to the questopm
-    //create a way for the user to input an answer (input box)
-    //the users response should be a string, even if contains numbers
-    //CANT BE ALERT OR PROMPT, will use an input box 
+//create a way for the user to input an answer (input box)
+//the users response should be a string, even if contains numbers
+//CANT BE ALERT OR PROMPT, will use an input box 
 //determine if answer is correct
-    //a correct answer awards one point and continues the game
-    //CAPILIZATION SHOULDNT MATTER, , adding to lowerCase() and tostring()
-    //give a message that they were right! NOT A PROMPT OR ALERT, will use document.write then setTimeout() to remove message after 5 seconds ,
-    //can remain for a few seconds then disappear either with a timer or a button
-    //after message is gone, give new question and place for user to answer worked in the loop
+//a correct answer awards one point and continues the game
+//CAPILIZATION SHOULDNT MATTER, , adding to lowerCase() and tostring()
+//give a message that they were right! NOT A PROMPT OR ALERT, will use document.write then setTimeout() to remove message after 5 seconds ,
+//can remain for a few seconds then disappear either with a timer or a button
+//after message is gone, give new question and place for user to answer worked in the loop
 //an incorrect answer resets the game and resets the score to 0, will use a if statement
 //after game ends give user a message NO PROMPT OR ALERT, 
 //give option to play again which resets game, using a button for "try again" once clicked using event listener will rerender code" 
 
 //keep track of and display a users score
-    //if correct answer add one point to total
-    //keep score until user answewrs incorrect answer use a loop to keep track of score:correct = =1 incorrect score = 0 then give play again button
+//if correct answer add one point to total
+//keep score until user answewrs incorrect answer use a loop to keep track of score:correct = =1 incorrect score = 0 then give play again button
 
 
 
 
 
+//✔️will need to create button on page 1 for starting game, title(h1) for both pages,for second page:  category(h2), total points(p), q(ptag), a(input box), submit button(to submit a)
+
+//✔️will need button for starting game:
+//button will be on first page, which takes to second page
+
+
+//✔️will need to use fetch to get categories and q's&a's
+// div id="questions" for q's
+//div id="answers"
+
+let theAnswer = "";
+let theQuestion
+let categoryId
+let jeopardyData
+let categoryTitle
+fetch("https://jservice.io/api/random/")
+    .then(response => response.json())
+    .then(data => {
+        jeopardyData = data[0];
+        theQuestion = jeopardyData.question;
+        getQuestionFromHTML.append(theQuestion);
+        theAnswer = jeopardyData.answer;
+        theAnswer.toString().toLowerCase
+        categoryTitle = jeopardyData.category.title;
+        getCategoryFromHTML.append(categoryTitle)
+        categoryId = jeopardyData.id;
+    })
+
+
+//will need to append one question to the p tag to show user the q
+//will be done after fetch and category is selected
+//will use querySelector and append results. 
+
+//will need an input box, that has a submit option to send the users answer to be checked
+//will use lowerCase() and tostring()
+
+
+//will need a button for submit
+//checks answer submitted with an event listener and rules
+
+//will need to if statement to either add to score or give message was wrong and try again with button for restart
+////if correct answer add one point to total
+//keep score until user answewrs incorrect answer use a if statement to keep track of score:correct = +1 incorrect score = 0 then give play again button
+
+
+submitButton.onclick = function (event) {
+    event.preventDefault();
+
+    if (getAnswerFromHTML === theAnswer) {
+        totalScore += 1;
+        getTotalScorefromHTML.append(totalScore)
+        document.write(`Good Job! Keep it going!`)
+        setInterval(document.write, 5000);
+        getQuestionFromHTML.push(theQuestion)
+    }
+    else {
+        totalScore = 0;
+        let endButton = document.createElement("Button");
+        endButton.addEventListener("click", function () {
+            endButton.innerHTML = `Sorry! The correct answer was: ${theAnswer}! You're final score was: ${totalScore}.Try Again!`;
+        });
 
 
 
-    //will need to create button on page 1 for starting game, title(h1) for both pages,for second page:  category(h2), total points(p), q(ptag), a(input box), submit button(to submit a)
+    }
+}
 
-
-    //will need button for starting game:
-        //button will be on first page, which takes to second page
-        //will need event listener to take to next page
-
-    //will need to use fetch to get categories and q's&a's
-    // div id="questions" for q's
-    //div id="answers"
-
-    //will need to append one question to the p tag to show user the q
-    //will be done after fetch and category is selected
-    //will use querySelector and append results. 
-
-    //will need an input box, that has a submit option to send the users answer to be checked
-    //will use lowerCase() and tostring()
-
-
-    //will need a button for submit
-        //checks answer submitted with an event listener and rules
-
-    //will need to if statement to either add to score or give message was wrong and try again with button for restart
-    ////if correct answer add one point to total
-    //keep score until user answewrs incorrect answer use a if statement to keep track of score:correct = +1 incorrect score = 0 then give play again button
-
-    //will need a button that allowes the code to rerender to start game play over again. 
+    //✔️will need a button that allows the code to rerender to start game play over again.
     //will be called if incorrect answer is called. 
 
-    //will need a if statement for totalScore 
+    //✔️will need a if statement for totalScore 
         //if answer is correct +1
-        //else message"Sorry that was incorrect". try again with a button. 
+        //else message"Sorry that was incorrect". try again with a button
