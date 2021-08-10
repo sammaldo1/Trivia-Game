c4fa256c6e991289be6be0913e3b1cb621cd65d6
Samantha <sammal729@gmail.com>
2021-08-09 21:11:27 -0400

    fixed fetch to get random q's everytime, add background and css, need to check for errors before submitting

--- code.js
+++ code.js
@@ -3,7 +3,6 @@
 
 // YOUR CODE HERE!
 
-
 let totalScore = 0;
 let totalPointsElement = document.getElementById("totalPoints");
 let questionsElement = document.getElementById("questions");
@@ -11,82 +10,82 @@ let categoryTitleElement = document.getElementById("categoryTitle");
 let submitButtonElement = document.getElementById("submitAnswer");
 let showMessageElement = document.getElementById("showMessage");
 let nextQuestionElement = document.getElementById("nextQuestion");
-let hideButton = document.getElementById("hideButton")
+let hideButton = document.getElementById("hideButton");
 let userAnswer = document.getElementById("userAnswer");
-let endButton = document.getElementById("endButton")
+let endButton = document.getElementById("endButton");
 let endMessageElement = document.getElementById("endMessage");
 nextQuestionElement.style.display = "none";
 hideButton.style.display = "none";
 
 let theAnswer = "";
-let theQuestion = ""
-let jeopardyData
-let categoryTitle
-let cluesToArray
-let randomq
-let randomqIndex
-let usedQuestions = []
+let theQuestion = "";
+let categoryTitle;
+let randomQuestion;
+let randomQuestionIndex;
+let usedQuestions = [];
+let fetchData;
+let arrayOfQuestions;
 
-fetch(`https://jservice.io/api/category/?id=${Math.floor(Math.random() * 100) + 1}`)
-    .then(response => response.json())
-    .then(data => {
-        jeopardyData = data.clues[0];
-        theQuestion = jeopardyData.question;
-        questionsElement.append(theQuestion);
-        theAnswer = jeopardyData.answer.toLowerCase().toString();
-        categoryTitle = data.title;
-        categoryTitleElement.append(categoryTitle)
-        submitButtonElement.addEventListener("click", checkAnswers);
+//pulls jeopardy questions from jservice API at random
+let fetchDataFromApi = fetch(
+  `https://jservice.io/api/category/?id=${Math.floor(Math.random() * 100) + 1}`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    fetchData = data;
+    arrayOfQuestions = data.clues;
+    randomQuestionIndex = Math.floor(Math.random() * arrayOfQuestions.length);
+    randomQuestion = arrayOfQuestions[randomQuestionIndex];
+    randomQuestion[Math.floor(Math.random() * fetchData.length)];
+    theQuestion = randomQuestion.question;
+    theAnswer = randomQuestion.answer.toLowerCase().toString();
+    questionsElement.append(theQuestion);
+    categoryTitle = data.title;
+    categoryTitleElement.append(categoryTitle);
+    submitButtonElement.addEventListener("click", checkAnswers);
+  });
 
+//Checks if users answer is correct for first question.
+//If correct adds a point to score, gives a congrads message , removes question from array, gives a button for next question.
+//If incorrect, resets score, gives try again message, and reloads page after 5 seconds.
+function checkAnswers(event) {
+  event.preventDefault();
 
-        function checkAnswers(event) {
-            event.preventDefault()
+  if (userAnswer.value.toString().toLowerCase() === theAnswer) {
+    totalScore += 1;
+    userAnswer.value = "";
+    totalPointsElement.innerHTML = `Total Points: <br>${totalScore}`;
+    showMessageElement.innerHTML = `Good Job! Keep it going! You're score right now is: ${totalScore}!`;
+    arrayOfQuestions.splice(randomQuestionIndex, 1);
+    setTimeout(function () {
+      showMessageElement.innerText = "";
+    }, 5000);
+    nextQuestionElement.style.display = null;
+    nextQuestionElement.addEventListener("click", nextQuestionsCheckAndpush);
+  } else {
+    endMessageElement.innerHTML = `Sorry! The correct answer was: ${theAnswer}! You're final score was: ${totalScore};`;
+    totalScore = 0;
+    totalPointsElement.innerHTML = `${totalScore}`;
+    hideButton.style.display = null;
+    endButton.addEventListener("click", function () {
+      setTimeout(function () {
+        endMessageElement.innerText = "";
+      }, 5000);
 
-            if (userAnswer.value.toString().toLowerCase() === theAnswer) {
-                totalScore += 1;
-                userAnswer.value = ""
-                totalPointsElement.innerHTML = `${totalScore}`
-                showMessageElement.innerHTML = `Good Job! Keep it going! You're score right now is: ${totalScore}!`;
-                setTimeout(function () {
-                    showMessageElement.innerText = "";
-                }, 5000);
-                nextQuestionElement.style.display = null;
-                nextQuestionElement.addEventListener("click", nextQuestionsCheckAndpush);
-            } else {
-                endMessageElement.innerHTML = `Sorry! The correct answer was: ${theAnswer}! You're final score was: ${totalScore};`
-                hideButton.style.display = null;
-                endButton.addEventListener("click", function () {
-                    setTimeout(function () {
-                        endMessageElement.innerText = "";
-                    }, 5000);
-                    window.location.reload()
-                })
+      window.location.reload();
+    });
+  }
+  //callback function for checkAnswers function.
+  // If answer is correct, generates a new question, removes the question from the array and allows for new random questions to generate on button click.
+  function nextQuestionsCheckAndpush(event) {
+    event.preventDefault();
 
-            }
-
-            function nextQuestionsCheckAndpush(event) {
-                event.preventDefault();
-
-                cluesToArray = Object.keys(data.clues);
-                randomqIndex = Math.floor(Math.random() * cluesToArray.length)
-                randomq = data.clues[cluesToArray[randomqIndex]];
-                randomq[Math.floor(Math.random() * data.length)];
-                theQuestion = randomq.question
-                theAnswer = randomq.answer.toLowerCase().toString();
-                questionsElement.innerHTML = theQuestion
-                usedQuestions.push(theQuestion)
-
-                if (userAnswer.value.toString().toLowerCase() === theAnswer) {
-                    totalScore + 1;
-                    totalPointsElement.innerHTML = `${totalScore}`
-                    showMessageElement.innerHTML = `Good Job! Keep it going! You're score right now is: ${totalScore}!`;
-                    setTimeout(function () {
-                        showMessageElement.innerText = "";
-                    }, 5000);
-
-                }
-
-            }
-        }
-    })
-
+    arrayOfQuestions = Object.keys(fetchData.clues);
+    randomQuestionIndex = Math.floor(Math.random() * arrayOfQuestions.length);
+    randomQuestion = fetchData.clues[arrayOfQuestions[randomQuestionIndex]];
+    randomQuestion[Math.floor(Math.random() * fetchData.length)];
+    theQuestion = randomQuestion.question;
+    theAnswer = randomQuestion.answer.toLowerCase().toString();
+    questionsElement.innerHTML = theQuestion;
+  }
+}
